a5d6762bba8c2ad723c841272181f9d100565bd3
ranelleandreir-gif <ranelleandreir@gmail.com>
2026-04-28 15:48:23 +0800

    Add files via upload

---
+++ admin-protection.js
@@ -0,0 +1,27 @@
+import { auth, db } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const snap = await getDoc(doc(db, "users", user.uid));
+
+  if (!snap.exists()) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const data = snap.data();
+
+  if (data.role !== "admin") {
+    alert("Access denied: Admin only");
+    window.location.href = "login.html";
+    return;
+  }
+
+});
